474640654a64496c2b78319a4004cf7df6c6dcc9
Kacper Lipiec <kacper.lipiec-ext@kering.com>
2025-02-11 23:53:24 +0100

    deleted fastapi

--- GET/get_fun.py
+++ GET/get_fun.py
@@ -5,8 +5,7 @@ import os
 from logger import IpLogger, log_function_call
 from exceptions_class import *
 import json
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+
 from decimal import Decimal
 
 def decimal_to_float(value):
@@ -19,8 +18,8 @@ def get_data(req: func.HttpRequest,cursor, **kwargs) -> func.HttpResponse:
     data = [dict(zip([column[0] for column in cursor.description], map(decimal_to_float, row))) for row in rows]
     return func.HttpResponse(
         json.dumps({"data": data}),
-        mimetype="application/json",
-        status_code = 200
+        headers={"Content-Type": "application/json"},
+        status_code=200
     )
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
